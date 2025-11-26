ecc07c5672936b134807ab8014d62d30071ca6c6
ElyasShamal <Elyas.shamal2@gmail.com>
2025-11-25 22:57:26 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letter.toLowerCase()))
+}
+
+function matchName(drivers, names) {
+    return drivers.filter(driver => driver.name.toLowerCase() === names.toLowerCase())
+}
